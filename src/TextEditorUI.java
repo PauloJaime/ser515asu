d8d560dba01820277c968cac9f2e23340b145a84
HongfeiJu <hongfeiju@gmail.com>
2018-09-18 17:53:25 -0700

    Add buttons in menu bar
    add File, Edit, Format and Help button. Add "Close file" action in file buttion

--- src/TextEditorUI.java
+++ src/TextEditorUI.java
@@ -1,19 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.*;
 
 
 public class TextEditorUI extends JFrame{
     private JTextArea textArea=new JTextArea();
     public TextEditorUI(){
         setSize(new Dimension(600, 400));
-        JScrollPane scrollPane=new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-        add(scrollPane);
+
         JMenuBar menuBar=new JMenuBar();
         setJMenuBar(menuBar);
-        JMenu file=new JMenu("Menu");
+        JMenu file=new JMenu("File");
         menuBar.add(file);
+        JMenu edit=new JMenu("Edit");
+        menuBar.add(edit);
+        JMenu format=new JMenu("Format");
+        menuBar.add(format);
+        JMenu help=new JMenu("Help");
+        menuBar.add(help);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
+
+
+        Action Close=new AbstractAction("Close file") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        };
+
+        file.add(Close);
+
     }
 
     public static void main(String[] args) {
