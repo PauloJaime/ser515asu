2723779860ba5d77ddca5d376eb4c08726653898
HongfeiJu <hongfeiju@gmail.com>
2018-09-19 18:56:01 -0700

    relocate

--- src/main/java/ui/TextEditorUI.java
+++ src/main/java/ui/TextEditorUI.java
@@ -1,7 +1,11 @@
+package main.java.ui;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.io.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class TextEditorUI extends JFrame {
 
@@ -24,8 +28,8 @@ public class TextEditorUI extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 JPanel jPanel=new JPanel();
                 jPanel.setLayout(new BorderLayout());
-                JButton closeTab=new JButton("close");
-                jPanel.add(closeTab);
+                JButton closeTab=new JButton("x");
+                jPanel.add(closeTab, BorderLayout.PAGE_END);
                 tabbedPane.addTab("new", jPanel);
                 JTextArea textArea=new JTextArea();
                 JScrollPane scrollPane=new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
@@ -36,7 +40,21 @@ public class TextEditorUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(fileChooser.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
-                    saveFile();
+                    String inputPath=fileChooser.getSelectedFile().getAbsolutePath();
+                    String filename=fileChooser.getSelectedFile().getName();
+
+                    JPanel jPanel=new JPanel();
+                    jPanel.setLayout(new BorderLayout());
+                    JButton closeTab=new JButton("close");
+                    jPanel.add(closeTab);
+                    tabbedPane.addTab(filename, jPanel);
+                    JTextArea textArea=new JTextArea();
+                    String content=getContent(inputPath);
+                    textArea.setText(content);
+                    JScrollPane scrollPane=new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+                    jPanel.add(scrollPane, BorderLayout.CENTER);
+
+                    //saveFile();
 
                 }
             }
@@ -45,7 +63,7 @@ public class TextEditorUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(fileChooser.showSaveDialog(null)==JFileChooser.APPROVE_OPTION){
-
+                    saveFile();
                 }
             }
         };
@@ -62,17 +80,14 @@ public class TextEditorUI extends JFrame {
             }
         };
 
-
         file.add(New);
         file.add(Open);
         file.add(Save);
         file.add(Close);
         file.add(Quit);
 
-
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
-
 
     }
 
@@ -81,6 +96,7 @@ public class TextEditorUI extends JFrame {
         try{
             fr=new FileReader(fileName);
             //textArea.read(fr,null);
+            //String content=read();
             fr.close();;
             setTitle(fileName);
         }catch(IOException e){
@@ -99,6 +115,9 @@ public class TextEditorUI extends JFrame {
             }
         }
     }
+    public String getContent(String filename){
+        return "haha";
+    };
 
     public static void main(String[] args) {
         TextEditorUI test=new TextEditorUI();
