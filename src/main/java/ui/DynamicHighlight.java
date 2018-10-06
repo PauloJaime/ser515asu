941d8a52eaf12941e5b9d388fe5bcadd7399afa4
HongfeiJu <hongfeiju@gmail.com>
2018-10-06 13:05:44 -0700

    change the color of text

--- src/main/java/ui/DynamicHighlight.java
+++ src/main/java/ui/DynamicHighlight.java
@@ -12,6 +12,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 
 public class DynamicHighlight implements Runnable {
@@ -22,40 +23,52 @@ public class DynamicHighlight implements Runnable {
     public void run() {
         this.textPane.addKeyListener(new KeyListener() {
             public void color(int i) {
+                Boolean blankEnd=false;
                 String text=textPane.getText();
-                //textPane.setText("");
+                if(text.charAt(text.length()-1)==' ') blankEnd=true;
+                if(blankEnd){
+                    String[] words=text.split(" ");
+                    int secondLastBlankIndex=textPane.getDocument().getLength()-words[words.length-1].length()-2;
+                    try{
+                        textPane.getDocument().remove(secondLastBlankIndex, textPane.getDocument().getLength() - secondLastBlankIndex);
+                    }catch(Exception e){
+                        throw null;
+                    }
+                    Random r=new Random();
+                    String preBlank=" ";
+                    if(words.length==1) preBlank="";
+                    appendToPane(textPane, preBlank+words[words.length-1]+" ",
+                            new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+                }
                 //Map<String, String> words=IOAgent.wordColorPairs(text);
 
-                Map<String, String> words=new HashMap<>();
-                words.put("variable", "#000000");
-                words.put("keyWord", "#FF0000");
-                words.put("function", "#0000FF");
-
-                switch(i){
-                    case 0:
-                        for(String word:words.keySet()){
-                            appendToPane(textPane, word, Color.decode(words.get(word)));
-                        }
-                    case 1:appendToPane(textPane, text, Color.decode("#FF0000"));break;
-                    case 2:
-                        JOptionPane.showMessageDialog(null,
-                            text, "change",
-                            JOptionPane.ERROR_MESSAGE);;break;
+                Map<String, Color> coloredWords=new HashMap<>();
+                /*
+                for(int j=0;j<words.length-1;j++){
+                    appendToPane(textPane, words[j]+" ", new Color(r.nextInt(red%256), r.nextInt(blue%256), r.nextInt(green%256)));
+                    red=red+20; blue=blue+20; green=green+20;
                 }
+                */
+
+                /*
+                for(String word:coloredWords.keySet()){
+                    appendToPane(textPane, word, coloredWords.get(word));
+                }
+                */
             }
             @Override
             public void keyTyped(KeyEvent e) {
-                color(2);
+
             }
 
             @Override
             public void keyPressed(KeyEvent e) {
-                color(0);
+
             }
 
             @Override
             public void keyReleased(KeyEvent e) {
-
+                color(0);
             }
         });
         /*
