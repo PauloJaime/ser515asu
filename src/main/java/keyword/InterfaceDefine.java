ee91ff6b94a574726091a475340b504f25191ad7
yiruhu <yiruhu@asu.edu>
2018-10-17 17:41:49 -0700

    US45-Task65: Modify the hard code

--- src/main/java/keyword/InterfaceDefine.java
+++ src/main/java/keyword/InterfaceDefine.java
@@ -1,13 +1,16 @@
 package keyword;
 
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import org.apache.commons.io.FileUtils;
 import org.json.JSONObject;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  * The class used to read color information
@@ -18,34 +21,40 @@ import java.io.IOException;
 public class InterfaceDefine {
     //public static void main(String args[]) throws IOException {
 	  public String matchColor(String keyWord) throws IOException {
-    	String color = "";
-        File file = new File("src/main/java/keyword.json");
-        String content= FileUtils.readFileToString(file,"UTF-8");
-        JSONObject Outer=new JSONObject(content);
-    	Map<String, Object> outerMap = new HashMap<>();
-    	Map<String, Object> innerMap = new HashMap<>();
-        for(Object k : Outer.keySet() ){
-        	Object v = Outer.get(k.toString());
-        	outerMap.put(k.toString(), v);
-        }
-    	
-        for (Entry<String, Object> entry : outerMap.entrySet()) {
-        	String str1 = entry.getValue().toString();
-        	JSONObject inner=new JSONObject(str1);
-            for(Object k : inner.keySet() ){
-            	Object v = inner.get(k.toString());
-            	innerMap.put(k.toString(), v);
-            }
-            for (Entry<String, Object> entry1 : innerMap.entrySet()){
-            	if(entry1.getValue().toString().equals(keyWord) ){
-            		color = entry.getKey();
-            		System.out.println(entry.getKey());
-            	}
-            }
+    	  String color = "";
+    	  String input = "";
 
-        	innerMap = new HashMap<>();
-        }
-        return color;  
-    }
+		  try {
+			  input = Thread.currentThread().getContextClassLoader().getResource("keyword.json").getPath();
+		  } catch (Exception e) {
+			  e.printStackTrace();
+		  }
+
+          String content= FileUtils.readFileToString(new File(input),"UTF-8");
+          JSONObject Outer=new JSONObject(content);
+    	  Map<String, Object> outerMap = new HashMap<>();
+    	  Map<String, Object> innerMap = new HashMap<>();
+          for(Object k : Outer.keySet() ){
+        	  Object v = Outer.get(k.toString());
+        	  outerMap.put(k.toString(), v);
+          }
+    	
+          for (Entry<String, Object> entry : outerMap.entrySet()) {
+        	  String str1 = entry.getValue().toString();
+        	  JSONObject inner=new JSONObject(str1);
+              for(Object k : inner.keySet() ){
+            	  Object v = inner.get(k.toString());
+            	  innerMap.put(k.toString(), v);
+              }
+              for (Entry<String, Object> entry1 : innerMap.entrySet()){
+            	  if(entry1.getValue().toString().equals(keyWord) ){
+            		  color = entry.getKey();
+            	  }
+              }
+
+        	  innerMap = new HashMap<>();
+          }
+          return color;
+      }
 }
 
