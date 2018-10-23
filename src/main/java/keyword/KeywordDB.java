2a5c85e3051180df46568879f94b4a3fedd9a267
魏资童 <weizitongbupt@gmail.com>
2018-10-22 22:35:18 -0700

    1. Add test folder and test res folder into build.gradle; 2. Fix IOAgent bug; 3. Refactor KeywordDB(prev: InterfaceDefine), US link: https://tree.taiga.io/project/paulo_jaime-ser515asu/us/45?milestone=204603; 4. Rename part of var in IntroFrame; 5. Finalize SyntaxAwareDocument, US link: https://tree.taiga.io/project/paulo_jaime-ser515asu/us/44?milestone=204603; 6. Add syntax sub-menuItem, US link: https://tree.taiga.io/project/paulo_jaime-ser515asu/taskboard/2nd-sprint-77 AND https://tree.taiga.io/project/paulo_jaime-ser515asu/us/45?milestone=204603; 7.Add Unit testing for PathDB

--- src/main/java/keyword/KeywordDB.java
+++ src/main/java/keyword/KeywordDB.java
@@ -8,16 +8,18 @@ import org.json.JSONObject;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * The class used to read color information
  *
  * @author Yiru Hu
- * @version 1.0
+ * @version 1.2
  */
 public class KeywordDB {
 	//public static void main(String args[]) throws IOException {
 	private Map<String,String> map;
+	private static final Logger log = Logger.getLogger("Log");
 
 	public KeywordDB() {
 	    this("Plain text");
@@ -25,30 +27,49 @@ public class KeywordDB {
 
 	public KeywordDB(String syntax) {
         map = new HashMap<>();
-        readKeywordInfoBySyntax("Java");
+        switchSyntax(syntax);
     }
 
-	private void readKeywordInfoBySyntax(String syntax) {
+    public Color matchColor(String keyWord) {
+        //String colorName = "";
+        Object key = ""+keyWord;
+        String colorName = map.getOrDefault(key, "Black");
+
+        Color color = Color.black;
+        if(colorName.equals("Yellow")){
+            color = Color.yellow;
+        }else if(colorName.equals("Blue")){
+            color = Color.blue;
+        }else if(colorName.equals("Green")){
+            color = Color.green;
+        }
+        return color;
+    }
+
+    public void switchSyntax(String syntax) {
 	    // Temporarily hard code to read Java keyword
+        map.clear();
 		String input = "";
 		try {
 			input = Thread.currentThread().getContextClassLoader().getResource(syntax + "Keyword.json").getPath();
 		} catch (NullPointerException e) {
+		    log.info("Cannot find syntax:" + syntax);
+		    log.info("By default, we regard this as plain text file");
 			return;
 		}
 
-        String content= null;
+        String content;
         try {
             content = FileUtils.readFileToString(new File(input),"UTF-8");
         } catch (IOException e) {
-            e.printStackTrace();
+            log.warning("Cannot find file: " + input);
+            throw new RuntimeException(e.getCause());
         }
 
-        JSONObject Outer=new JSONObject(content);
+        JSONObject jsonObject = new JSONObject(content);
 		Map<String, Object> outerMap = new HashMap<>();
-		Map<String, Object> innerMap = new HashMap<>();
-		for(Object k : Outer.keySet() ){
-			Object v = Outer.get(k.toString());
+		for(Object k : jsonObject.keySet() ){
+			Object v = jsonObject.get(k.toString());
 			outerMap.put(k.toString(), v);
 		}
 
@@ -57,31 +78,11 @@ public class KeywordDB {
 			JSONObject inner = new JSONObject(str1);
 			for(Object k : inner.keySet() ){
 				Object v = inner.get(k.toString());
-				innerMap.put(k.toString(), v);
-			}
-
-			for (Entry<String, Object> entry1 : innerMap.entrySet()){
-				map.put(entry1.getValue().toString(), entry.getKey());
+                map.put(v.toString(), entry.getKey());
 			}
 
 		}
 
-	}
-
-	public Color matchColor(String keyWord) {
-		//String colorName = "";
-		Object key = ""+keyWord;
-		String colorName = map.getOrDefault(key, "Black");
-
-		Color color = Color.black;
-		if(colorName.equals("Yellow")){
-			color = Color.yellow;
-		}else if(colorName.equals("Blue")){
-			color = Color.blue;
-		}else if(colorName.equals("Green")){
-			color = Color.green;
-		}
-		return color;
 	}
 
 }
