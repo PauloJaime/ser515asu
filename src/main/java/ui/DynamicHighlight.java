bd8d0e4a52c3be4ca2dc0b98d4690627a43b5acf
HongfeiJu <hongfeiju@gmail.com>
2018-10-22 17:28:25 -0700

    US36-Task41: remove travial code

--- src/main/java/ui/DynamicHighlight.java
+++ src/main/java/ui/DynamicHighlight.java
@@ -48,18 +48,6 @@ public class DynamicHighlight implements Runnable {
                 //Map<String, String> words=IOAgent.wordColorPairs(text);
 
                 Map<String, Color> coloredWords=new HashMap<>();
-                /*
-                for(int j=0;j<words.length-1;j++){
-                    appendToPane(textPane, words[j]+" ", new Color(r.nextInt(red%256), r.nextInt(blue%256), r.nextInt(green%256)));
-                    red=red+20; blue=blue+20; green=green+20;
-                }
-                */
-
-                /*
-                for(String word:coloredWords.keySet()){
-                    appendToPane(textPane, word, coloredWords.get(word));
-                }
-                */
             }
             @Override
             public void keyTyped(KeyEvent e) {
@@ -76,45 +64,7 @@ public class DynamicHighlight implements Runnable {
                 color(0);
             }
         });
-        /*
-        textPane.getDocument().addDocumentListener(new DocumentListener() {
             public void color(int i) {
-                String text=textPane.getText();
-                //textPane.setText("");
-                //Map<String, String> words=IOAgent.wordColorPairs(text);
-
-                Map<String, String> words=new HashMap<>();
-                words.put("variable", "#000000");
-                words.put("keyWord", "#FF0000");
-                words.put("function", "#0000FF");
-
-                switch(i){
-                    case 0:textPane.setText("");
-                        for(String word:words.keySet()){
-                            appendToPane(textPane, word, Color.decode(words.get(word)));
-                        }
-                    case 1:textPane.setText("");appendToPane(textPane, text, Color.decode("#FF0000"));break;
-                    case 2:JOptionPane.showMessageDialog(null,
-                            text, "change",
-                            JOptionPane.ERROR_MESSAGE);;break;
-                }
-            }
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                color(0);
-            }
-
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                color(1);
-            }
-
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                color(2);
-            }
-        });
-        */
     }
     private void appendToPane(JTextPane pane, String text, Color color) {
 
