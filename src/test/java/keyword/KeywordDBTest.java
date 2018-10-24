f26f522d75303ef519b86b5aa3edbe2a73efd804
Binbin Yan <562657743@qq.com>
2018-10-23 19:23:54 -0700

    US45-Task57: unit testing

---
+++ src/test/java/keyword/KeywordDBTest.java
@@ -0,0 +1,20 @@
+package keyword;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * This is the test file for KeywordDB
+ *
+ * @author Binbin Yan
+ * @version 1.0
+ */
+
+public class KeywordDBTest {
+    @Test
+    public void testswitchSyntax(){
+        KeywordDB test = new KeywordDB("Java");
+
+
+    }
+}
